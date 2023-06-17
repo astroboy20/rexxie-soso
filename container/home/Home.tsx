a3d940d71cd36fa70a11c5dbe061f2757fef07fa
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-06-17 18:15:53 +0100

    set up to get random names

--- container/home/Home.tsx
+++ container/home/Home.tsx
@@ -1,18 +1,42 @@
 import { Button } from "@/components/Button";
 import { CustomText } from "@/components/CustomText";
-import { Box, Input } from "@chakra-ui/react";
+import { Box, Input, Spinner } from "@chakra-ui/react";
 import { Arrow } from "../../asset";
 import { useRouter } from "next/router";
 import { ButtonStyle, HomeSubText } from "../maincontainer/layout.style";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const HomeContainer = () => {
   const router = useRouter();
   const handleLink = () => {
     router.push("./introduction");
   };
+
+  const [randomName, setRandomName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchRandomName = () => {
+    setIsLoading(true);
+    axios
+      .get("https://rexxie-soso.onrender.com/name")
+      .then((response:any) => {
+        setRandomName(response);
+        setIsLoading(false); 
+        console.log(response)
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false); 
+      });
+  };
+
+  useEffect(() => {
+    fetchRandomName();
+  }, []); 
+
   return (
     <>
-      
       <div className="header">
         <CustomText variant="h1" type="primary" weight="300">
           Hey REXXI & SOSO HERE
@@ -21,21 +45,24 @@ const HomeContainer = () => {
 
       <HomeSubText>
         <CustomText variant="h3" type="primary" weight="400">
-          What’s your name?
+          What’s your name? {randomName}
         </CustomText>
 
-        <Input
-          padding="14px"
-          placeholder="Tell us your name"
-          _placeholder={{ fontSize: "14px", color: "#B7B7B7" }}
-          marginBottom="2.5%"
-        />
-
-        <Button size="normal" variant="primary" onClick={handleLink}>
-          <ButtonStyle>
-            Continue <Arrow />
-          </ButtonStyle>
-        </Button>
+        {isLoading ? (
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+            />
+        ) : (
+          <Button size="normal" variant="primary" onClick={handleLink}>
+            <ButtonStyle>
+              Continue <Arrow />
+            </ButtonStyle>
+          </Button>
+        )}
       </HomeSubText>
     </>
   );
